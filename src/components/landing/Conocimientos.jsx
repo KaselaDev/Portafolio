6e5d2ef1f4edfc6ad330cf0b6bca2cc71fd07e1a
KaselaDev <santiagocasellas2005@gmail.com>
2025-02-04 16:22:25 -0300

    Fix Knowledge 1.0

--- src/components/landing/Conocimientos.jsx
+++ src/components/landing/Conocimientos.jsx
@@ -1,87 +1,87 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "@/style/conocimientos.css";
-import Icon from "@/utils/Icon"
-import motorMaster from "@/img/motorMasterLogo.svg"
+import Icon from "@/utils/Icon";
+import motorMaster from "@/img/motorMasterLogo.svg";
+import linux from "@/img/linuxLogo.svg";
+import bashScript from "@/img/bashScriptLogo.svg";
+import mui from "@/img/muiLogo.svg";
+import figma from "@/img/figmaLogo.svg";
 import { useSelector } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+
+// Datos de los conocimientos
+const slides = [
+  { css: "html", icon: "faHtml5", link: "https://developer.mozilla.org/es/docs/Web/HTML"},
+  { css: "css", icon: "faCss", link: "https://developer.mozilla.org/es/docs/Web/CSS"},
+  { css: "js", icon: "faSquareJs", link: "https://developer.mozilla.org/es/docs/Web/JavaScript"},
+  { css: "mui", img: mui, link: "https://mui.com/"},
+  { css: "react", icon: "faReact", link: "https://es.react.dev/"},
+  { css: "php", icon: "faPhp", link: "https://php.net"},
+  { css: "sql", icon: "faDatabase", typeIcon: "solid", link: "https://www.mysql.com/"},
+  { css: "bashScript", img: bashScript, link: "https://www.hostinger.es/tutoriales/bash-script-linux"},
+  { css: "git", icon: "faGit", link: "https://git-scm.com/"},
+  { css: "figma", img: figma, link: "https://www.figma.com/"},
+  { css: "trello", icon: "faTrello", link: "https://trello.com/"},
+  { css: "node", icon: "faNode", link: "https://nodejs.org/es"},
+  { css: "linux", img: linux, link: "https://www.linux.org/"},
+  // { css: "motorMaster", img: motorMaster, link: ""},
+];
 
 const Conocimientos = () => {
   const lenguage = useSelector((state) => state.lenguage);
+  const [cantSliders, setCantSlider] = useState(getSlidesPerView());
+
+  function getSlidesPerView() {
+    if (window.innerWidth >= 1500) return 7;
+    if (window.innerWidth >= 1200) return 6;
+    if (window.innerWidth >= 900) return 5;
+    if (window.innerWidth >= 750) return 4;
+    if (window.innerWidth >= 600) return 3;
+    return 2;
+  }
+
+  const handleResize = useCallback(() => {
+    setCantSlider(getSlidesPerView());
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
 
   return (
     <section className="stats">
-      <div className="espaciadorSections" id="Conocimientos"/>
-      <h2 className="subTitulo">{lenguage === "ES" ? "Conocimientos" : "Knowledge"}</h2>
-      <br />
+      <div className="espaciadorSections" id="Conocimientos" />
+      <h2 className="subTitulo">
+        {lenguage === "es" ? "Conocimientos / Herramientas" : "Knowledge / Tools"}
+      </h2>
       <div className="contentStats">
-        {/* Front-end */}
-        <div className="stat">
-          <div className="titulo">
-            <h3>Front-end</h3>
-          </div>
-          <div className="html tag">
-            <Icon type="brands" icon="faHtml5" />
-            <h4>HTML</h4>
-          </div>
-          <div className="css tag">
-            <Icon type="brands" icon="faCss3Alt" />
-            <h4>CSS</h4>
-          </div>
-          <div className="js tag">
-            <Icon type="brands" icon="faSquareJs" />
-            <h4>JavaScript</h4>
-          </div>
-        </div>
-
-        {/* Back-end */}
-        <div className="stat">
-          <div className="titulo">
-            <h3>Back-end</h3>
-          </div>
-          <div className="php tag">
-            <Icon type="brands" icon="faPhp" />
-            <h4>PHP</h4>
-          </div>
-          <div className="sql tag">
-            <Icon type="solid" icon="faDatabase" />
-            <h4>SQL</h4>
-          </div>
-          <div className="bash tag">
-            <Icon type="solid" icon="faCube" />
-            <h4>Bash Script</h4>
-          </div>
-        </div>
-
-        {/* Framework */}
-        <div className="stat">
-          <div className="titulo">
-            <h3>Framework</h3>
-          </div>
-          <div className="react tag">
-            <Icon type="brands" icon="faReact" />
-            <h4>React</h4>
-          </div>
-          <div className="motorMaster tag">
-            <img className="iconEspecial" src={motorMaster} alt="MotorMaster.php" />
-            <h4>MotorMaster</h4>
-          </div>
-        </div>
-
-        {/* Otros */}
-        <div className="stat">
-          <div className="titulo">
-            <h3>{lenguage === "ES" ? "Otros" : "Others"}</h3>
-          </div>
-          <div className="git tag">
-            <Icon type="brands" icon="faGitAlt" />
-            <h4>Git</h4>
-          </div>
-          <div className="figma tag">
-            <Icon type="brands" icon="faFigma" />
-            <h4>Figma</h4>
-          </div>
-          <div className="trello tag">
-            <Icon type="brands" icon="faTrello" />
-            <h4>Trello</h4>
+        <div className="statsContainer">
+          <div className="boxs">
+            <Swiper
+              slidesPerView={cantSliders}
+              spaceBetween={10}
+              loop={true}
+              autoplay={{ delay: 0, disableOnInteraction: false }}
+              speed={3000}
+              allowTouchMove={true}
+              modules={[Autoplay, Navigation]}
+            >
+              {slides.map((slide, index) => (
+                <SwiperSlide key={index}>
+                  <a href={slide.link} target="_blank" className={`box Icon${slide.css}`} title="Ir a la pagina oficial">
+                    {slide.img ? (
+                      <img src={slide.img} alt={`${slide.css} logo`} />
+                    ) : (
+                      <Icon type={slide.typeIcon ? slide.typeIcon : "brands"} icon={slide.icon} />
+                    )}
+                  </a>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
